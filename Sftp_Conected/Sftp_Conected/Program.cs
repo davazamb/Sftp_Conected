cc6c85393250e38ac6a5e8cee05a387e4adef500
davaz <davazamb10@outlook.com>
2021-08-10 09:11:31 -0400

    Example connected ftp with tamir.sharpssh

---
+++ Sftp_Conected/Sftp_Conected/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sftp_Conected
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Sftp_Conected/Sftp_Conected1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Tamir.SharpSsh;
+
+namespace Sftp_Conected1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConectarSFTP();
+        }
+
+        private static void ConectarSFTP()
+        {
+            string Hostname = "ftp-server.demo.solarwinds.com";
+            string dir_path = "demo";
+            string Username = "demo";
+            string Password = "demo";
+
+            Sftp sftpClient = new Sftp(Hostname, Username, Password);
+
+            try
+            {
+                Console.Write("Connecting...");
+                sftpClient.Connect(21);
+                if (sftpClient.Connected)
+                {
+
+                    Console.WriteLine("OK");
+                }
+                else
+                {
+
+                    Console.WriteLine("NO OK");
+                }
+            }
+            catch (Exception EX)
+            {
+
+                throw;
+            }
+        }
+    }
+}
